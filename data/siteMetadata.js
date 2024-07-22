c3f88d07f7bd2b7c36262fef8f369407a5454809
core <dzh213@163.com>
2024-07-22 18:35:39 +0800

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Gulu Blog',
   author: 'gulu',
   headerTitle: 'Gulu Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  //description: 'A blog created with Next.js and Tailwind.css',
   language: 'zh-CN',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
